8bf0466e2f326018943f6c28316ccbfd804db326
EthanJ <e_jutila@fanshaweonline.ca>
2023-04-01 19:45:25 -0400

    Updated Image sizes

--- js/team.js
+++ js/team.js
@@ -9,7 +9,7 @@ let members = {
     ethan: {
         name: 'Ethan Jutila',
         headline: "Ethan Jutila - Front-End Developer",
-        bio: 'Some information about Thor',
+        bio: 'Ethan is from Thunder Bay Ontario and moved to London Ontario in search of new experiences, adventure, and knowledge. He plays for the Fanshawe FUEL eSports team for a game called VALORANT. Ethan also loves to play guitar and perform and produce music for others. He likes all kinds of music, but his favourite genres consist of anything metal or rock related. But he is open to all forms of musical interpretations. ',
         avatar: '../jutila_e_quimosing_j_dynamicteampage/images/ethan-background.jpg'
     }
 };
